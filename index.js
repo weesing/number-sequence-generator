289801d0d61369616e2c5c8c0fd452c4b3e09209
weesing <posttomichael@gmail.com>
2020-02-05 20:01:06 +0800

    Make minimum and maximum editable.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+const MIN = 1;
+const MAX = 49;
 const MAX_LENGTH = 6;
+
+//Bubble sort
+var bsortAsc = function(inpArr) {
+  for(var i = MAX_LENGTH - 1; i >= 0; --i) {
+    for(var j = 1; j <= i; ++j) {
+      if (inpArr[j-1] > inpArr[j]) {
+        var temp = inpArr[j-1];
+        inpArr[j-1] = inpArr[j];
+        inpArr[j] = temp;
+      }
+    }
+  }
+  return inpArr;
+}
+
+var range = (MAX + 1) - MIN;
 var num = [];
+
 for(var i = 0 ; i < MAX_LENGTH; i++) {
-  num[i] = ((Math.random() * 1000000) % 50) + 1
+  num[i] = ((Math.random() * 1000) % range);
+  num[i] = num[i] + MIN;
   num[i] = Math.floor(num[i]);
   for (var j = 0 ; j < i; ++j) {
     if(num[i] == num[j]) {
@@ -11,17 +31,9 @@ for(var i = 0 ; i < MAX_LENGTH; i++) {
   }
 }
 
-//Bubble sort
-for(var i = MAX_LENGTH - 1; i >= 0; --i) {
-  for(var j = 1; j <= i; ++j) {
-    if (num[j-1] > num[j]) {
-      var temp = num[j-1];
-      num[j-1] = num[j];
-      num[j] = temp;
-    }
-  }
-}
-
+//Sort ascending
+bsortAsc(num);
+console.log("Generated numbers: ");
 var str = "";
 for(var i = 0; i < MAX_LENGTH; ++i) {
   str = str + num[i] + " ";
